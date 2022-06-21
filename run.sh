b39ba55bdb4cd53240f352fc411bd79034a44e1e
Hendrix <hendrix04@gmail.com>
2022-06-21 15:55:25 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Start Backend
-cd /app
-sh ./mealie/run.sh
-
 # Start Frontend
 # If the source would change the frontend
 # code to just use the standard shell script
 cd /app/frontend
 caddy start --config /app/frontend/Caddyfile
-yarn start -p 3001
+yarn start -p 3001 &
+
+# Start Backend
+cd /app
+sh ./mealie/run.sh
